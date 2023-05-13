0b986f524f8437cfcc83b46e3ea770271bb89bdd
V. SAI DAIWIK <113048796+SaiDaiwikV@users.noreply.github.com>
2023-05-13 10:54:12 +0530

    Add files via upload

---
+++ Program1.c
@@ -0,0 +1,33 @@
+// In this program we learn : 'What is the Syntax of the C-programming and How does it Works'.
+
+// First we want to import basic libraries to run the simple C-program.
+
+// To import we will use '#include<lib.h>' line.
+
+// Program starts : 
+
+#include<stdio.h>
+
+// The above line indicates that we have successfully imported 'stdio.h' library. But we want know that is called 'stdio' it means that stdio stands for 'Standard input and output'.
+
+int main()
+// int main is the function where we write the code. we required minimum of one function in C to run the code;
+{
+    //  to enter the int main we use '{}';
+
+    printf("Hello, World");
+
+    // printf() is used to print the value what we written in the double dots on the output screen;
+
+    // Do not forgot to keep semi-colon at the end of the programe if not then it shows an error;
+
+    // As we want to become a good programer to we want to use 'return 0' in the end of the program;
+
+    return 0;
+
+    // This is will help in exiting of the program after compleing all the line of the code;
+}
+
+// use the VS-CODE to write the programes and create a seperate folder to save all the codes which will help for referrence;
+
+// Congrats you have successfully completed your first C-program;
