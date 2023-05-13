179616c01b0b302ac30c70852db0565058ec486a
V. SAI DAIWIK <113048796+SaiDaiwikV@users.noreply.github.com>
2023-05-13 14:21:59 +0530

    Add files via upload

---
+++ Program2.c
@@ -0,0 +1,28 @@
+// In previous Programe1 we have learnt what is the syntax and how to rnn the code 
+
+// Let us start with course with some deeper knowledge
+
+// Step-1 : Import Header files;
+
+#include<stdio.h>
+
+// Start with the main function : 
+
+int main()
+{
+    // using of multiple printf functions in the main function;
+
+    printf("Hello\n");
+
+    // '\n' is used because when we use next printf both will print seprate lines;
+    // if do not use '\n' both the printed lines will come in the same line in the output without any space;
+
+    printf("My name is V.Sai Daiwik\n");
+
+    printf("Presently I am styding at LPU\n");
+
+    return 0;
+
+    // return 0 is an optional thing without using it program will run without any error;
+    
+}
